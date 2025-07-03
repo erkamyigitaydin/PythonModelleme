5a30085f8229a10d9a7de497d6496f0231c4361e
erkamyigitaydin <erkamyigitaydin@gmail.com>
2025-07-03 16:42:04 +0300

    ML

--- test.py
+++ test.py
@@ -1,10 +1,11 @@
 from PIL import Image
 from core import extract_text_from_image, image_preprocess, extract_info_from_slip
+import argparse
 
-def main():
+def main(image_path):
     try:
-        image_path = "/Users/erkamaydin/Desktop/Python/myocr/slip.jpg"
         sonuc = extract_info_from_slip(image_path)
+        print("sonuc:", sonuc["sonuc"])
         print("İşyeri No:", sonuc["isyeri_no"])
         print("Terminal No:", sonuc["terminal_no"])
         print("Tutar:", sonuc["tutar"])
@@ -12,5 +13,9 @@ def main():
         print("Adres:", sonuc["adres"])
     except Exception as e:
         print(f"Error: {e}")
+
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="Extract slip information from an image")
+    parser.add_argument("image_path", help="Path to the slip image")
+    args = parser.parse_args()
+    main(args.image_path)
